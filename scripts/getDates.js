97402c0c181c56cb02ca221626c2a36442373cfb
JosuMarinho <del22031@byui.edu>
2024-03-03 02:44:26 -0600

    update

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,12 +1,5 @@
-// Get Current Year
-var currentYear = new Date().getFullYear();
-
-// Set element with id year to current year
-document.getElementById("year").innerHTML = currentYear;
-
-
-// Get Last Modified Date
-var lastModified = document.lastModified;
-
-// Set element with id lastModified to last modified date
-document.getElementById("lastModified").innerHTML = 'Last Modification: '+lastModified;
+function lastedit () {
+    var latestDate = new Date(document.lastModified);
+    document. getElementById ("placeholder"). innerHTML = late
+}
+    
