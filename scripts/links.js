5f81baa33f188020d61d1630da4751bb0757a8a3
JosuMarinho <del22031@byui.edu>
2024-04-07 23:02:05 -0600

    update links.js

--- scripts/links.js
+++ scripts/links.js
@@ -12,40 +12,32 @@ const getLinks = async function() {
 }
 
 const displayLinks = function(data) {
-    // Borra el contenido existente del contenedor principal
     mainGrindContainer.innerHTML = '';
 
-    // Crea una única sección para todas las semanas
     let sectionEl = document.createElement('section');
-    sectionEl.classList.add('card'); // Agrega la clase 'card' al elemento <section>
+    sectionEl.classList.add('card');
 
-    // Itera sobre cada elemento en el array 'data'
     for (let index = 0; index < data.length; index++) {
-        // Crea un encabezado <h2> para mostrar el título de la semana
+    
         let h2El = document.createElement('h2');
-        h2El.textContent = data[index].week; // Establece el texto del encabezado como el título de la semana
-        sectionEl.appendChild(h2El); // Agrega el encabezado al elemento <section>
+        h2El.textContent = data[index].week; 
+        sectionEl.appendChild(h2El); 
 
-        // Crea una lista <ul> para contener los enlaces
+
         let ulEl = document.createElement('ul');
 
-        // Itera sobre cada enlace en la semana actual
         data[index].links.forEach((link) => {
-            // Crea un elemento <li> para cada enlace
             let liEl = document.createElement('li');
-            // Crea un enlace <a> con la URL generada a partir de 'baseURL' y el enlace específico de la semana
             let linkEl = document.createElement('a');
-            linkEl.href = `${baseURL}/${link.url}`; // La URL completa se forma concatenando 'baseURL' y la URL específica del enlace
-            linkEl.textContent = link.title; // Establece el texto del enlace como el título del enlace
-            linkEl.target = '_blank'; // Abre el enlace en una nueva pestaña
-            liEl.appendChild(linkEl); // Agrega el enlace al elemento <li>
-            ulEl.appendChild(liEl); // Agrega el elemento <li> a la lista <ul>
+            linkEl.href = `${baseURL}/${link.url}`; 
+            linkEl.textContent = link.title; 
+            linkEl.target = '_blank'; 
+            liEl.appendChild(linkEl);
+            ulEl.appendChild(liEl);
         });
 
-        // Agrega la lista <ul> al elemento <section>
         sectionEl.appendChild(ulEl);
     }
 
-    // Agrega la única sección al contenedor principal 'mainGrindContainer' al final
     mainGrindContainer.appendChild(sectionEl);
 };
